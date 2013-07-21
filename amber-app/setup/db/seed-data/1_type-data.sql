7af86325095d62acee45733f7101763bf37634b8
Rakesh Sinha <sinhar@vmware.com>
2013-07-20 19:39:04 -0700

    Added parent data for metadata.

--- amber-app/setup/db/seed-data/1_type-data.sql
+++ amber-app/setup/db/seed-data/1_type-data.sql
@@ -11,12 +11,12 @@ INSERT INTO `metadata` (`id`, `name`, `description`, `type`) VALUES (2, 'Science
 INSERT INTO `metadata` (`id`, `name`, `description`, `type`) VALUES (3, 'Mathematics', 'Contains mathematics books.', 'CATEGORY');
 
 --metadata for sub-category
-INSERT INTO `metadata` (`id`, `name`, `description`, `type`) VALUES (4, 'Computer Engineering', 'Contains computer engineering books.', 'SUB_CATEGORY');
-INSERT INTO `metadata` (`id`, `name`, `description`, `type`) VALUES (5, 'Electronics Engineering', 'Contains electronics engineering books.', 'SUB_CATEGORY');
-INSERT INTO `metadata` (`id`, `name`, `description`, `type`) VALUES (6, 'Elemectrical Engineering', 'Contains electrical engineering books.', 'SUB_CATEGORY');
-INSERT INTO `metadata` (`id`, `name`, `description`, `type`) VALUES (7, 'Physics', 'Contains physics books.', 'SUB_CATEGORY');
-INSERT INTO `metadata` (`id`, `name`, `description`, `type`) VALUES (8, 'Chemistry', 'Contains chemistry books.', 'SUB_CATEGORY');
-INSERT INTO `metadata` (`id`, `name`, `description`, `type`) VALUES (9, 'Biology', 'Contains biology books.', 'SUB_CATEGORY');
-INSERT INTO `metadata` (`id`, `name`, `description`, `type`) VALUES (10, 'Arthemetics', 'Contains arthematics books.', 'SUB_CATEGORY');
-INSERT INTO `metadata` (`id`, `name`, `description`, `type`) VALUES (11, 'Trigonometry', 'Contains trigonometry books.', 'SUB_CATEGORY');
-INSERT INTO `metadata` (`id`, `name`, `description`, `type`) VALUES (12, 'Alzebra', 'Contains alzebra books.', 'SUB_CATEGORY');
+INSERT INTO `metadata` (`id`, `name`, `description`, `type`, `parent_id`) VALUES (4, 'Computer Engineering', 'Contains computer engineering books.', 'SUB_CATEGORY', 1);
+INSERT INTO `metadata` (`id`, `name`, `description`, `type`, `parent_id`) VALUES (5, 'Electronics Engineering', 'Contains electronics engineering books.', 'SUB_CATEGORY', 1);
+INSERT INTO `metadata` (`id`, `name`, `description`, `type`, `parent_id`) VALUES (6, 'Elemectrical Engineering', 'Contains electrical engineering books.', 'SUB_CATEGORY', 1);
+INSERT INTO `metadata` (`id`, `name`, `description`, `type`, `parent_id`) VALUES (7, 'Physics', 'Contains physics books.', 'SUB_CATEGORY', 2);
+INSERT INTO `metadata` (`id`, `name`, `description`, `type`, `parent_id`) VALUES (8, 'Chemistry', 'Contains chemistry books.', 'SUB_CATEGORY', 2);
+INSERT INTO `metadata` (`id`, `name`, `description`, `type`, `parent_id`) VALUES (9, 'Biology', 'Contains biology books.', 'SUB_CATEGORY', 2);
+INSERT INTO `metadata` (`id`, `name`, `description`, `type`, `parent_id`) VALUES (10, 'Arthemetics', 'Contains arthematics books.', 'SUB_CATEGORY', 3);
+INSERT INTO `metadata` (`id`, `name`, `description`, `type`, `parent_id`) VALUES (11, 'Trigonometry', 'Contains trigonometry books.', 'SUB_CATEGORY', 3);
+INSERT INTO `metadata` (`id`, `name`, `description`, `type`, `parent_id`) VALUES (12, 'Alzebra', 'Contains alzebra books.', 'SUB_CATEGORY', 3);
